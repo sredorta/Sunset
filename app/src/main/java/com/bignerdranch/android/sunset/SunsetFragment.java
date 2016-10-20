71c41363f4c0c80a888c97325d4cb63d38239ef1
sredorta <sergi.redorta@hotmail.com>
2016-10-20 15:32:46 +0200

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -0,0 +1,76 @@
+package com.bignerdranch.android.sunset;
+
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+
+/**
+ * Created by sredorta on 10/20/2016.
+ */
+public class SunsetFragment extends Fragment {
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_sunset,container,false);
+        mSceneView = v;
+        mSunView   = v.findViewById(R.id.sun);
+        mSkyView   = v.findViewById(R.id.sky);
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAnimation();
+            }
+        });
+
+        return v;
+    }
+
+    private void startAnimation() {
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        //Make the sun go down
+        ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd).setDuration(3000);
+        heightAnimator.setInterpolator(new AccelerateInterpolator());
+
+        //Change sky colors for sunset
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator.ofInt(mSkyView,"backgroundColor",mBlueSkyColor,mSunsetSkyColor).setDuration(1500);
+        //sunsetSkyAnimator.setInterpolator(new AccelerateInterpolator());
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        //Sky colors to night
+        ObjectAnimator nightSkyAnimator = ObjectAnimator.ofInt(mSkyView,"backgroundColor",mSunsetSkyColor,mNightSkyColor).setDuration(1500);
+        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(heightAnimator).with(sunsetSkyAnimator).before(nightSkyAnimator);
+        animatorSet.start();
+        //heightAnimator.start();
+        //sunsetSkyAnimator.start();
+    }
+}
